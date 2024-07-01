4871e001bc6799fdeebf3e1e421e5d9741015f07
Ash1sh-B <156790016+Ash1sh-B@users.noreply.github.com>
2024-07-01 21:26:30 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-          console.writeline("programming challenge number 2 fibonacci
+          console.writeline("programming challenge number 2 fibonacci");
+            int num1 = 1
+            int num2 = 2
+            Console.writeline(num1);
+            console.writeline(num2);
+            total = 0
+            while (num2<4000000);
+            num2 = num1 + num2
+            num1 = num2
+            
+            IF (num2 /2 = 0);
+            {
+              total = total +num2
+            }
+            ENDIF
+            ENDWHILE
         }
     }
 }
