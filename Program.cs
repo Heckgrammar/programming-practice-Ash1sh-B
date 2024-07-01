68611d430724f2ba5197784249b2b52fbce23770
Ash1sh-B <156790016+Ash1sh-B@users.noreply.github.com>
2024-07-01 21:29:20 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,9 +9,10 @@
             int num2 = 2
             Console.writeline(num1);
             console.writeline(num2);
-            total = 0
+           int total = 0
+            int num3 = 0
             while (num2<4000000);
-            num2 = num1 + num2
+            num3 = num1 + num2
             num1 = num2
             
             IF (num2 /2 = 0);
