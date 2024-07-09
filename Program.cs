22eddd6fa4fc452cf396efc4c803ebb0d432aa11
Ash1sh-B <156790016+Ash1sh-B@users.noreply.github.com>
2024-07-09 21:09:12 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,16 +4,16 @@
     {
         static void Main(string[] args)
         {
-          console.writeline("programming practice challenge");
-            int max = 1000;
-            int total = 0;
-            for (int i = 0; i < max; i++
-            (
-                if (i % 3 == 0 || i % 5 == 0)
-                (
-                    total += i;
-                )
-                console.writeline("The sum of all the numbers that are divisible by 3 and 5 is: " + sum")
+          // console.writeline("programming practice challenge");
+          //   int max = 1000;
+          //   int total = 0;
+          //   for (int i = 0; i < max; i++
+          //   (
+          //       if (i % 3 == 0 || i % 5 == 0) divides by 3 and 5
+          //       (
+          //           total += i; adds the numbers together 
+          //       )
+          //       console.writeline("The sum of all the numbers that are divisible by 3 and 5 is: " + sum") outputs the number
                 
                 
   
