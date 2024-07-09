ad71c044f81737e73ce87d38d95d0961716d3008
Ash1sh-B <156790016+Ash1sh-B@users.noreply.github.com>
2024-07-09 21:07:01 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,23 +4,19 @@
     {
         static void Main(string[] args)
         {
-          console.writeline("programming challenge number 2 fibonacci");
-            int num1 = 1
-            int num2 = 2
-            Console.writeline(num1);
-            console.writeline(num2);
-           int total = 0
-            int num3 = 0
-            while (num2<4000000);
-            num3 = num1 + num2
-            num1 = num2
-            
-            IF (num2 /2 = 0);
-            {
-              total = total +num2
-            }
-            ENDIF
-            ENDWHILE
+          console.writeline("programming practice challenge");
+            int max = 1000;
+            int total = 0;
+            for (int i = 0; i < max; i++
+            (
+                if (i % 3 == 0 || i % 5 == 0)
+                (
+                    total += i;
+                )
+                console.writeline("The sum of all the numbers that are divisible by 3 and 5 is: " + sum")
+                
+                
+  
         }
     }
 }
